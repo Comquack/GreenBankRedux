afc4afad7d349224c12ec1722e8d21a61de6f3ae
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2018-07-17 12:17:27 +1000

    Tranfer Project

---
+++ MyGreenBank/app/src/androidTest/java/com/mygreenbank/mygreenbank/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.mygreenbank.mygreenbank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by John on 4/09/2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onButtonClick() throws Exception {
+
+    }
+
+}
